35fab0748afa43aaf1cbac0c9ee4ba7f538dfd38
suyu-booth <suyu@chicagobooth.edu>
2024-12-13 00:34:08 +0000

    completed image upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,9 +2,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -44,4 +45,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg"
   # end
+
+
 end
